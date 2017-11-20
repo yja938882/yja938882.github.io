3552c8ccb914dd8d7310e61eb7f86ae6654398d3
yja938882 <wndks93882@>
2017-11-20 16:05:44 +0900

    excluse

--- world/world.js
+++ world/world.js
@@ -1,28 +1,77 @@
-      var myGeoJSONPath = './custom.geo.json';
-        var myCustomStyle = {
-            stroke: true,
-            color: '#E8B71A',
-            weight: 2,
-            fill: true,
-            fillColor: '#1FDA9A',
-            fillOpacity: 1
+var cusdiv_visible = true;
+function customonoff(){
+ if(cusdiv_visible){
+  $('#customdiv').hide();
+    cusdiv_visible = false;
+  }else{
+    $('#customdiv').show();
+    cusdiv_visible = true;
+ }
+}
+
+var myGeoJSONPath = './custom.geo.json';
+var map;
+var layer;
+
+ var pre_markers = [
+   {
+     "name": "캐나다",
+     "lat": 56.130366,
+     "lng": -106.346771
+   },
+   {
+     "name": "앵귈라",
+     "lat": 18.220554,
+     "lng": -63.068615
+   },
+   {
+     "name": "일본",
+     "lat": 36.204824,
+     "lng": 138.252924
+   }
+]; 
+//function initStyle :  처음 Style 초기화 
+var defaultStyle = { stroke: true, color: '#E8B71A', weight: 2,fill: true,fillColor: '#1FDA9A',fillOpacity: 1};
+var exclusiveStyle ={ stroke:true, color: '#eef', weight: 1, fill:true, fillColor: '#eef', fillOpacity: 1};
+function initStyle(feature){
+  switch(feature.properties.name){
+    case "Russia":
+    case "China":
+      return exclusiveStyle;
+    default :
+      return defaultStyle;
+    break;
+  }
+}
+function customClickable(feature){
+          if(feature.properties.name=="Russia"){
+            return false;
+        }else{
+            return true;
         }
-        var map;
-        var layer;
-        $(function(){
-            $.getJSON(myGeoJSONPath,function(data){
-            map = L.map('map').setView([45.74739, -50], 1);
 
-            layer = L.geoJson(data, {
-                clickable: true,
-                style: myCustomStyle,
-                onEachFeature: onEachFeature
-            }).addTo(map);
+    }
 
-     
+$(function(){
+  $.getJSON(myGeoJSONPath,function(data){
+    map = L.map('map').setView([45.74739, 0], 1.5);
+    layer = L.geoJson(data, {
+      clickable: customClickable,
+      style: initStyle,
+      onEachFeature: onEachFeature
+  }).addTo(map);
+
+for ( var i=0; i < pre_markers.length; ++i ) {
+     L.marker( [pre_markers[i].lat, pre_markers[i].lng] )
+      .bindPopup( '<a href="' + pre_markers[i].name + '" target="_blank">' + pre_markers[i].name + '를 클릭하셨습니다'+ '</a>' )
+      .addTo( map );
+  }
 
 
         });
+
+
+
         });
 
         function onClickChLineColor(){
@@ -41,6 +90,12 @@
         }
 
         function onEachFeature(feature, layer) {
+            if(feature.properties.name =="Russia"||
+              feature.properties.name=="China"){
+                return;
+              }
+    
+ 
              layer.on({
                 mouseover : mouseOverM,
                 mouseout :mouseOutM,
@@ -52,6 +107,9 @@
 
         function mouseOverM(e){
             var t = e.target;
+            if(t.feature.properties.name=="Russia"){
+                return;
+            }
             t.setStyle({
                 color:$('#mo_line_color_input').val(),
                 fillColor:$('#mo_ct_color_input').val()
@@ -72,16 +130,20 @@
 
         var isZoom = false;
         function zoomToFeature(e) {
-
-
+          console.log(e.target.feature.properties.name);
             if(!isZoom){
                  map.fitBounds(e.target.getBounds());
                  isZoom=true;
             }else if( isZoom ==true){
-                map.fitBounds(layer.getBounds());
+             //   map.fitBounds(layer.getBounds());
+                map.setView([45.74739, 0], 1.5);
                 isZoom=false;
             }
         }
         function AddMarker(){
             L.marker([$('#l1').val(), $('#l2').val()]).addTo(map);
         }
+
+
+   
+
